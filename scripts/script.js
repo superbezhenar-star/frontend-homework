eadde58de35c319134d653ccd0039a766eab371a
iulia1 <superbezhenar@gmail.com>
2025-10-08 21:42:57 +0300

    Add initial HTML page and JavaScript file with basic functionality
    - Created 'opagina.html' with a basic HTML structure and linked JavaScript file.
    - Added 'script.js' containing various variable declarations and console logs for testing.
    - Included alert to demonstrate JavaScript execution.

---
+++ scripts/script.js
@@ -0,0 +1,51 @@
+alert("HEELLO I AM JS") //comentariu chemarea functiei
+//HEELLO I AM JS-parametrul ftiei
+console.log("si eu sunt in consola")
+let a = 5;
+let c;
+c="this message is from a variable"
+var b;
+b=77;
+const h=99;
+let u=h+b*120
+u=h+b*34
+console.log(u)
+console.log(c)
+console.log(a)  
+console.log(a)
+
+
+
+
+
+
+
+
+
+
+
+
+let f = "6"
+let r = 7
+let j = 11
+console.log(Number(f))//rez6
+
+console.log(f+r)//67
+console.log(f - r)//-1
+
+console.log(r++)//8
+console.log(++r)
+console.log(r--)//6
+
+
+j -= r//4
+j += r//11
+j *= r//77
+
+r+=j//84
+r -=j//7
+j/=r//11
+
+r-=j//-4
+j-=r//15
+
